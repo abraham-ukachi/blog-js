8928f3bfa3c5cd3f48af73dbd089572b2f365f2e
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-03-09 07:09:49 +0100

    Create autoloader w/ composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'abraham-ukachi/blog-js',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '558e8b43cb7cbf23a5c6c6484cdb2368997815de',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abraham-ukachi/blog-js' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '558e8b43cb7cbf23a5c6c6484cdb2368997815de',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
