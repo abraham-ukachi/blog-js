5709e601a2ebd3b736e32d846740c31d4e535abd
abraham-ukachi <abraham.ukachi@laplateforme.io>
2023-03-27 18:52:15 +0200

    Create home-page.js

---
+++ src/pages/home-page.js
@@ -0,0 +1,237 @@
+/* 
+* @license
+* blog-js
+* Copyright (c) 2023 Abraham Ukachi. The BlogJS Project Contributors. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* @name: Home Page
+* @type: script
+* @author: Abraham Ukachi <abraham.ukachi@laplateforme.io>
+* @contributor: Hajar Aslan <hajar.aslan@laplateforme.io>
+*
+* Example usage:
+*   1-|> import { HomePage } from 'src/pages/home-page.js';
+*    -|> 
+*    -|> // get the root of all 'pages' from `blogJSApp`
+*    -|> const root = blogJSApp.getRootOf(APP_PAGES);
+*    -|> const controller = HomePage;
+*    -|> 
+*    -|> // instantiate the `HomePage`
+*    -|> let type = 'main'; // or aside
+*    -|> let homePage = new HomePage(type, 'home-page');
+*    -|> 
+*    -|> // Open the home page
+*    -|> homePage.open();
+*    -|> 
+*    -|> }
+*
+*/
+
+import { html } from '../Engine.js';
+import { Page } from '../Page.js';
+
+"use strict"; 
+// ^^^^^^^^^ This keeps us on our toes, as it forces us to use all pre-defined variables, among other things 😅
+
+
+// Defining some constant variables...
+
+
+
+// Create a `HomePage` class
+export class HomePage extends Page {
+
+  /**
+   * Properties
+   *
+   * @type { Object }
+   */
+  static get properties() {
+    return {
+      updated: { type: Boolean },
+      opened: { type: Boolean }
+    };
+  }
+
+  /**
+   * Styles
+   *
+   * @type { Array }
+   */
+  static get styles() {
+    return ['home'];
+  }
+
+
+  /**
+   * Animations
+   *
+   * @type { Array }
+   */
+  static get animations() {
+    return [ 'fade-in', 'slide-from-down' ];
+  }
+
+
+  /**
+   * Pages
+   *
+   * @type { Array[Object] }
+   */
+  static get pages() {
+    return [
+      { name: 'home', views: ['default'] }
+    ];
+  }
+
+  
+  // Define some public properties
+   
+  // Define some private properties
+  
+   
+
+  /**
+   * Constructor of the Page
+   *
+   * @param { String } type
+   * @param { String } name
+   */
+  constructor(type = 'main', name = 'home-page') {
+    // call the `Page` constructor with `Page` as it's controller
+    super(type, name);
+
+    // set default attributes
+
+
+    // DEBUG [4dbsmaster]: tell me about it ;)
+    // console.log(`[constructor]: #_props.init => `, this.#_props.init);
+  }
+
+
+  /**
+   * Method that is called from the Page's constructor
+   * @override from `Page`
+   */
+  init() {
+    // Initialize public properties
+    this.updated = false;
+    this.opened = false;
+    
+    // Initialize private properties
+    
+  }
+
+  
+  /**
+   * Method used to render this home page 
+   * @override from `Page`
+   */
+  render() {
+    return html`
+      <!-- Home Page Container -->
+      <div id="homePageContainer" class="flex-layout vertical centered" fit>
+        <p class="txt upper">hello from <strong>${this.name}</strong></p>
+
+        <!-- Views -->
+        <div id="views"></div>
+        <!-- End of Views -->
+      </div>
+      <!-- End of Home Page Container -->
+    `;
+  }
+
+
+  /**
+   * First time this page gets updated 
+   * @override from `Page`
+   */
+  firstUpdated() {
+
+    // add event listeners here
+
+    console.log(`\x1b[33m[firstUpdated]: ${this.name} has been updated #firstTime ;)\x1b[0m`);
+  }
+
+  /**
+   * Handler that is called whenever a property changes
+   *
+   * @param { Array[Object] } changedProperties
+   * @override from `Page`
+   */
+  propertiesUpdated(changedProperties) {
+    super.propertiesUpdated(changedProperties);
+    
+    // changedProperties.forEach((prop) => {});
+  }
+
+  
+
+  /* >> Public Methods << */
+
+
+  /**
+   * Handler that is called when the home page is ready
+   */
+  onReady() {
+    // DEBUG [4dbsmaster]: tell me about it ;)
+    console.info(`\x1b[36m[onReady]: ${this.name} is ready`); 
+  }
+
+
+  /**
+   * Handler that is called when the home page is open 
+   */
+  onOpen() {
+    // DEBUG [4dbsmaster]: tell me about it ;)
+    console.info(`\x1b[37m[onOpen]: ${this.name} is open`); 
+  }
+  
+
+  /**
+   * Handler that is called when the home page is hidden 
+   */
+  onClose() {
+    // DEBUG [4dbsmaster]: tell me about it ;)
+    console.info(`\x1b[37m[onClose]: ${this.name} is closed`); 
+  }
+
+
+  /* >> Public Setters << */
+
+  /* >> Public Getters << */
+
+
+
+  /* >> Private Methods << */
+
+  /* >> Private Setters << */
+
+  /* >> Private Getters << */
+
+
+
+}; // <- End of `HomePage` class
+
+
+// Attach a behavior to `HomePage`...
+// Object.assign(HomePage.prototype, HomePageBehavior);
+
+
